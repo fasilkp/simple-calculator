6bde426641db372062a351dc2c1e680a18d43cb1
Mohamed Fasil <fasilkp314@gmail.com>
2021-07-08 09:07:10 +0530

    error fixed

--- script.js
+++ script.js
@@ -17,8 +17,15 @@ function calculate(val){
         if(val=='+' || val=='-' || val=='*' || val=='/'){
             operation=true
         }
-    
-       
+        if(screen.value[0]==0){
+            if(screen.value[1]=="1" || screen.value[1]=="2" ||screen.value[1]=="3" ||screen.value[1]=="4" || screen.value[1]=="5" || screen.value[1]=="6" || screen.value[1]=="7" ||screen.value[1]=="8" || screen.value[1]=="9" ||screen.value[1]=="0" ||screen.value[1]=="(" ||screen.value[1]==")"){
+                screen.value=val
+            }
+        }
+        else if(screen.value[0]=="*" || screen.value[0]=="/" || screen.value[0]==")"){
+            screen.value=val;
+        }
+     
 }
 function clearScr(){
     screen.value="";
